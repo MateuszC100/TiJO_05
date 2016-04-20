b851231259156fd69ef2c993f3f8f03abc06f793
MateuszC100 <mateuszcierniak102@o2.pl>
2016-04-20 11:11:03 +0200

    app.spec.js

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -30,6 +30,17 @@ describe('app', function () {
         });
     });
 
+    describe('toHaveBeenisPalindrome',function(){
+        beforeAll(function(){
+            spyOn(app,'isPalindrome');
+            app.isPalindrome('zaraz');
+        });
+        it('counting vowels',function(){
+            expect(app.isPalindrome).toHaveBeenCalled();
+            expect(app.isPalindrome).toHaveBeenCalledWith('zaraz');
+        });
+    });
+
     describe('vowelCount', function () {
 
         describe('toHaveBeenCalled', function () {
@@ -49,8 +60,20 @@ describe('app', function () {
         });
 
         describe('calls.count()', function () {
-
         });
     });
+
+    describe('toHaveBeenvowelCount',function(){
+        beforeAll(function(){
+            spyOn(app,'vowelCount');
+            app.vowelCount('dalej');
+        });
+        it('counting vowels',function(){
+            expect(app.vowelCount).toHaveBeenCalled();
+            expect(app.vowelCount).toHaveBeenCalledWith('dalej');
+        });
+    });
+
+
 });
 
